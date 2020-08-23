676d896654d776ead4e17a21d416915c952d7452
Rsja12 <asocarras10@gmail.com>
2020-08-23 15:33:37 -0400

    create Card presentational component to make styles more reusable

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,20 +1,44 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+import Card from '../components/Card'
 
 const StartGameScreen = (props) => {
     return (
         <View style={styles.screen}>
-            <Text>Game Screen</Text>
+            <Text style={styles.title}>Start A New Game!</Text>
+            <Card style={styles.inputContainer}>
+                <Text>Enter a number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button title='Reset' color='red' />
+                    <Button title='Confirm' />
+                </View>
+            </Card>
         </View>
-    )
-}
+    );
+};
 
-export default StartGameScreen
+export default StartGameScreen;
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
         padding: 10,
-        alignItems: 'center'
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: '80%',
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
     }
-})
+});
