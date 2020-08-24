1f2fe4f1a7ce4b7acc1353a23224cfc1a2c84a50
Rsja12 <asocarras10@gmail.com>
2020-08-24 16:49:05 -0400

    add logic to determine which screen to render. Create content for GameScreen component

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+    const [userNum, setUserNum] = useState()
+
+    const handleStartGame = (selectedNum) => {
+        setUserNum(selectedNum)
+    }
+
+    let content = <StartGameScreen onStartGame={handleStartGame} />
+    if (userNum) {
+        content = <GameScreen userNum={userNum} />
+    }
+
     return (
         <View style={styles.screen}>
             <StatusBar style='auto' />
 
             <Header title='Guess' />
-            <StartGameScreen />
+            {content}
         </View>
     );
 }
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import NumberContainer from '../components/NumberContainer';
+import Card from '../components/Card'
 
 const generateRandomNum = (min, max, exclude) => {
     min = Math.ceil(min);
@@ -13,14 +16,38 @@ const generateRandomNum = (min, max, exclude) => {
 };
 
 const GameScreen = (props) => {
-    const [currentGuess, setCurrentGuess] = useState(generateRandomNum(1, 100, props.userNum))
+    const [currentGuess, setCurrentGuess] = useState(
+        generateRandomNum(1, 100, props.userNum)
+    );
+
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.screen}>
+            <Text style={styles.guessText}>AI guess</Text>
+            <NumberContainer>{currentGuess}</NumberContainer>
+            <Card style={styles.buttonContainer}>
+                <Button title='Lower' />
+                <Button title='Higher' />
+            </Card>
         </View>
     );
 };
 
 export default GameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+        width: 300,
+        maxWidth: '80%'
+    },
+    guessText: {
+        fontSize: 22
+    }
+});
