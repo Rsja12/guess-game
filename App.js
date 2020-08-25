0811a332068ed26c89baba4c4a1e7ad76db87d5b
Rsja12 <asocarras10@gmail.com>
2020-08-25 15:59:00 -0400

    integrate AppLoading component from expo to handle loading fonts before rendering component

--- App.js
+++ App.js
@@ -1,35 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
 
+const fetchFonts = () => {
+    return Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+};
+
 export default function App() {
-    const [userNum, setUserNum] = useState()
-    const [rounds, setRounds] = useState(0)
+    const [userNum, setUserNum] = useState();
+    const [rounds, setRounds] = useState(0);
+    const [isDataLoaded, setIsDataLoaded] = useState(false);
+
+    if (!isDataLoaded) {
+        return (
+            <AppLoading
+                startAsync={fetchFonts}
+                onFinish={() => setIsDataLoaded(true)}
+                onError={(error) => console.log(error)}
+            />
+        );
+    }
 
     const handleNewGame = () => {
-        setRounds(0)
-        setUserNum(null)
-    }
+        setRounds(0);
+        setUserNum(null);
+    };
 
     const handleStartGame = (selectedNum) => {
-        setUserNum(selectedNum)
-        setRounds(0)
-    }
+        setUserNum(selectedNum);
+        setRounds(0);
+    };
 
     const handleGameOver = (numOfRounds) => {
-        setRounds(numOfRounds)
-    }
+        setRounds(numOfRounds);
+    };
 
-    let content = <StartGameScreen onStartGame={handleStartGame} />
+    let content = <StartGameScreen onStartGame={handleStartGame} />;
     if (userNum && rounds <= 0) {
-        content = <GameScreen userNum={userNum} onGameOver={handleGameOver} />
+        content = <GameScreen userNum={userNum} onGameOver={handleGameOver} />;
     } else if (rounds > 0) {
-        content = <GameOverScreen rounds={rounds} onRestart={handleNewGame} />
+        content = <GameOverScreen rounds={rounds} onRestart={handleNewGame} />;
     }
 
     return (
